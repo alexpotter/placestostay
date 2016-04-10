e905f0e94b08fcb23b66caed5ffacfbe633116c7
Alex Potter <alex.potter1993@gmail.com>
2016-04-10 18:39:16 +0100

    Alter lat long

--- Resources/Templates/index.php
+++ Resources/Templates/index.php
@@ -35,7 +35,7 @@
 
         function initMap() {
             var map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: -34.397, lng: 150.644},
+                center: {lat: 51.5074, lng: 0.1278},
                 zoom: 6
             });
             var infoWindow = new google.maps.InfoWindow({map: map});
