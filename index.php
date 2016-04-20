872d73ce153e943f0199b5ac7a14bc74bb69c9c8
Alex Potter <alex.potter1993@gmail.com>
2016-04-20 19:00:33 +0100

    Tidy code

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 include('app/Config.php');
 include('app/http/Request.php');
-include ('app/Controllers/Controller.php');
+include('app/Controllers/Controller.php');
 include('app/Controllers/Api.php');
 include('app/Controllers/PlacesToStay.php');
 include('app/Controllers/Admin.php');
