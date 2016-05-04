84d5021c9a70e174373cdc3f9ff2c4d3188b7b30
Alex Potter <alex.potter1993@gmail.com>
2016-05-04 13:02:23 +0100

    Display all rooms availible in info window

--- Resources/Templates/index.php
+++ Resources/Templates/index.php
@@ -10,13 +10,14 @@
     <script type="text/javascript" src="<?php echo $this->fileUrl('bootstrap/js/bootstrap.js'); ?>"></script>
     <!-- Custom scripts -->
     <link rel="stylesheet" href="<?php echo $this->fileUrl('dist/css/style.css'); ?>">
-    <script type="text/javascript" src="<?php echo $this->fileUrl('dist/js/frontend.js'); ?>"></script>
 
     <!-- Date Stuff -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     <script>
         $(function() {
+            localStorage.clear();
+
             $( "#dateFrom" ).datepicker();
             $( "#dateTo" ).datepicker();
         });
@@ -105,11 +106,134 @@
                 handleLocationError(false, infoWindow, map.getCenter());
             }
 
-            $('#results').on('click', '#viewOnMap', function (e) {
+            $('#searchPlace').submit(function(e) {
                 e.preventDefault();
 
-                var location = JSON.parse(localStorage.getItem("location" + this.getAttribute('data-value')));
+                infoWindow.close();
 
+                var dateFrom = $('#dateFrom').val().split('/');
+                dateFrom = dateFrom[2] + '-' + dateFrom[0] + '-' + dateFrom[1];
+
+                var dateTo = $('#dateTo').val().split('/');
+                dateTo = dateTo[2] + '-' + dateTo[0] + '-' + dateTo[1];
+
+                var url = $( this ).prop( 'action' ) +
+                    '/' + $('#town').val() + '/'
+                    + dateFrom + '/' + dateTo + '?api_key=c2f3851b4fc9d0f';
+
+                $.get(url).done(function (rooms) {
+                    $('#error').hide().html();
+
+                    rooms = $.parseJSON(rooms);
+
+                    var html = '\
+                    <table class="table" style="margin-top: 20px;">\
+                        <tr>\
+                            <th>Location</th>\
+                            <th>Type</th>\
+                            <th>Room Description</th>\
+                            <th>Beds</th>\
+                            <th>Price per night</th>\
+                            <th>View on map</th>\
+                            <th>View dates and book</th>\
+                        <tr/>\
+                    ';
+
+                    var count = 0;
+                    $.each(rooms, function (keys, params) {
+                        $.each(params, function(key, param) {
+
+                            var name = param.name;
+                            var type = param.location_type;
+
+                            localStorage.setItem("location" + param.ID, JSON.stringify(param));
+                            console.log(JSON.parse(localStorage.getItem("location" + param.ID)));
+
+                            // Drop pins
+                            var location = {lat: parseFloat(param.lat), lng: parseFloat(param.lng)};
+
+                            var contentString = '\
+                            <div id="content">\
+                                <div id="siteNotice">\
+                                </div>\
+                                <h1 id="firstHeading" class="firstHeading">' + param.name + '</h1>\
+                                <div id="bodyContent">\
+                                    <div class="row">';
+
+                            if (count == 0)
+                            {
+                                map.setCenter(location);
+                                map.setZoom(14);
+                            }
+
+                            $.each(param.rooms, function(key1, param1) {
+                                html += '\
+                                    <tr>\
+                                        <td>' + name + '</td>\
+                                        <td>' + type + '</td>\
+                                        <td>' + param1.room_description + '</td>\
+                                        <td>' + param1.number_of_beds + '</td>\
+                                        <td> £ ' + parseFloat(param1.room_price / 100).toFixed(2) + '</td>\
+                                        <td><a href="#" class="viewOnMap" data-value="' + param.ID + '">View on map</a></td>\
+                                        <td><a href="#" class="viewAndBook" data-value="' + param1.ID + '">View and Book</a></td>\
+                                    </tr>\
+                                ';
+
+                                contentString += '\
+                                <div class="col-md-4">\
+                                    <p>\
+                                        '+ param1.room_description + '\
+                                    </p>\
+                                    <p>\
+                                        Number of beds: ' + param1.number_of_beds +'\
+                                        <br>\
+                                        Price per night: ' + param1.room_price + '\
+                                    <p>\
+                                        <button class="btn btn-success viewAndBook" data-value="' + param1.ID + '">Book now</button>\
+                                    </p>\
+                                </div>';
+
+                                localStorage.setItem("room" + param1.ID, JSON.stringify(param1));
+                                console.log(JSON.parse(localStorage.getItem("room" + param1.ID)));
+                            });
+
+                            contentString += '\
+                                    </div>\
+                                </div>\
+                            </div>';
+
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: contentString
+                            });
+                            var marker = new google.maps.Marker({
+                                position: location,
+                                map: map,
+                                title: 'Location'
+                            });
+                            marker.addListener('click', function() {
+                                infoWindow.open(map, marker);
+                            });
+
+                            count ++;
+                        });
+                    });
+
+                    html += '</table>';
+
+                    $('#results').html(html);
+                })
+                .fail(function(jqXHR, status, thrownError) {
+                    var responseText = jQuery.parseJSON(jqXHR.responseText);
+                    $('#error').show().html('\
+                        <div class="alert alert-danger">' + responseText.message + '</div>\
+                    ');
+                });
+            });
+
+            // View on map
+            $('#results').on('click', '.viewOnMap', function (e, infoWindows) {
+                e.preventDefault();
+                var location = JSON.parse(localStorage.getItem("location" + this.getAttribute('data-value')));
                 geocodePlaceId(geocoder, map, infoWindow, location.google_id);
             });
         }
@@ -142,6 +266,12 @@
                 }
             });
         }
+        
+        $(function () {
+            $('#results').on('click', '#viewAndBook', function (e) {
+                e.preventDefault();
+            });
+        });
     </script>
     <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuK5rdDsSZpXyi5VBjW7g8N1IJUtAXZwA&callback=initMap">
