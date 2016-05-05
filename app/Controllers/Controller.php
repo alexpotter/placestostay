d3fbc0d06dd85f80a369c7b6fd27a9d324ab4519
Alex Potter <alex.potter1993@gmail.com>
2016-05-05 23:46:31 +0100

    Give visitcolerado access

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -59,6 +59,9 @@ class Controller
     public function returnJson($array, $statusCode)
     {
         header('Access-Control-Allow-Methods: GET, POST');
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+        header("Access-Control-Allow-Credentials: true ");
         header('Content-Type: application/json');
         http_response_code($statusCode);
         echo json_encode($array);
