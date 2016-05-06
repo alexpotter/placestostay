6b5f54913774d5c32bf0a0c884d0ab8c0fc4453a
Alex Potter <alex.potter1993@gmail.com>
2016-05-06 02:48:41 +0100

    Display assignment on index page

--- Resources/Templates/index.php
+++ Resources/Templates/index.php
@@ -11,6 +11,7 @@
     <!-- Custom scripts -->
     <link rel="stylesheet" href="<?php echo $this->fileUrl('dist/css/style.css'); ?>">
     <link rel="stylesheet" href="<?php echo $this->fileUrl('dist/css/calendar.css'); ?>">
+    <link href='https://fonts.googleapis.com/css?family=Source+Code+Pro' rel='stylesheet' type='text/css'>
 
     <!-- Date Stuff -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
@@ -24,6 +25,8 @@
             $( "#dateTo" ).datepicker();
         });
     </script>
+
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
 </head>
 <body>
 <div id="splash-index">
@@ -35,7 +38,7 @@
             </p>
             <p class="sub-message">
                 <a class="start-search" data-scroll href="#searchContainer">Start Search</a>
-                <a class="start-search" data-scroll href="#searchContainer">View Assignment</a>
+                <a class="start-search" data-scroll href="#assignmentContainer">View Assignment</a>
             </p>
         </div>
     </div>
@@ -84,6 +87,263 @@
             <div id="results">
 
             </div>
+        </div>
+    </div>
+</div>
+<div id="assignmentContainer">
+    <div class="row">
+        <div class="col-xs-6">
+            <h1>1) The web service must be able to look up locations</h1>
+            <p>Url:
+                <span class="code">
+                    <a href="http://placestostay.alexpotter.dev/apotter/index.php/api/search/London/2016-05-01/2016-05-31?api_key=dc45c373b4c92bc">
+                        http://placestostay.alexpotter.dev/apotter/index.php/api/search/London/2016-05-01/2016-05-31?api_key=dc45c373b4c92bc
+                    </a>
+                </span>
+            </p>
+            <p>And:
+                <span class="code">
+                    <a href="<?php echo $this->url('api/search/'); ?>London/2016-05-01/2016-05-31/Hotel?api_key=dc45c373b4c92bc">
+                        <?php echo $this->url('api/search/'); ?>London/2016-05-01/2016-05-31/Hotel?api_key=dc45c373b4c92bc
+                    </a>
+                </span>
+            </p>
+        </div>
+        <div class="col-xs-6">
+            <h1>Returns</h1>
+            <pre class="prettyprint">
+{
+  locations: [
+    {
+      ID: "2",
+      name: "Premier Inn London Waterloo",
+      lat: "51.501748",
+      lng: "-0.11648600000000897",
+      google_id: "ChIJ_75-fbgEdkgRp4ql6Rq76ao",
+      street_number: "85",
+      address_line1: "York Road",
+      town: "London",
+      postcode: "SE1 7NJ",
+      country: "United Kingdom",
+      location_type: "Hotel",
+      belongs_to: "1",
+      rooms: [
+        {
+          ID: "1",
+          room_description: "2 beds with a view",
+          location_id: "2",
+          number_of_beds: "2",
+          room_price: "2800",
+          bookedDates: [
+            {
+              start: "2016-05-07",
+              end: "2016-05-09"
+            },
+            {
+              start: "2016-05-10",
+              end: "2016-05-12"
+            }
+          ]
+        },
+        {
+          ID: "2",
+          room_description: "3 beds with a view",
+          location_id: "2",
+          number_of_beds: "3",
+          room_price: "3000",
+          bookedDates: [
+            {
+              start: "2016-05-20",
+              end: "2016-05-23"
+            },
+            {
+              start: "2016-05-10",
+              end: "2016-05-12"
+            },
+            {
+              start: "2016-05-13",
+              end: "2016-05-16"
+            },
+            {
+              start: "2016-05-17",
+              end: "2016-05-18"
+            },
+            {
+              start: "2016-05-08",
+              end: "2016-05-09"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      ID: "3",
+      name: "Premier Inn London Leicester Square",
+      lat: "51.51104650000001",
+      lng: "-0.13018379999994067",
+      google_id: "ChIJJUyxFNIEdkgRajb-0tlYT_0",
+      street_number: "1",
+      address_line1: "Leicester Square",
+      town: "London",
+      postcode: "WC2H 7BP",
+      country: "United Kingdom",
+      location_type: "Hotel",
+      belongs_to: "1",
+      rooms: [
+        {
+          ID: "3",
+          room_description: "3 beds with a view",
+          location_id: "3",
+          number_of_beds: "3",
+          room_price: "3500",
+          bookedDates: [
+
+          ]
+        },
+        {
+          ID: "4",
+          room_description: "2 beds with a view",
+          location_id: "3",
+          number_of_beds: "2",
+          room_price: "3000",
+          bookedDates: [
+
+          ]
+        }
+      ]
+    },
+    {
+      ID: "4",
+      name: "Premier Inn London Blackfriars",
+      lat: "51.512878",
+      lng: "-0.1055149999999685",
+      google_id: "ChIJ24SRYq0EdkgRAksghbg1UM8",
+      street_number: "1",
+      address_line1: "Dorset Rise",
+      town: "London",
+      postcode: "EC4Y 8EN",
+      country: "United Kingdom",
+      location_type: "Hotel",
+      belongs_to: "1"
+    },
+    {
+      ID: "5",
+      name: "Premier Inn London Southwark Borough Market",
+      lat: "51.5071442",
+      lng: "-0.09287050000000363",
+      google_id: "ChIJgXlxzVcDdkgRWi5_YPdOOf4",
+      street_number: "34",
+      address_line1: "Park Street",
+      town: "London",
+      postcode: "SE1 9EF",
+      country: "United Kingdom",
+      location_type: "Hotel",
+      belongs_to: "1",
+      rooms: [
+        {
+          ID: "6",
+          room_description: "Room for two with a view",
+          location_id: "5",
+          number_of_beds: "2",
+          room_price: "4500",
+          bookedDates: [
+
+          ]
+        }
+      ]
+    }
+  ]
+}
+            </pre>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-6">
+            <h1>2) Web service must be able to make a booking</h1>
+            <p><a data-scroll href="#searchContainer">Try here</a></p>
+        </div>
+        <div class="col-xs-6">
+            <h1>Service returns if successful</h1>
+            <pre class="prettyprint">
+{
+  "message": "Booking successfully created",
+  "booking": {
+    "ID": "14",
+    "room_id": "3",
+    "date_from": "2016-06-03",
+    "date_to": "2016-06-06",
+    "user_id": "1",
+    "price_paid": "10500",
+    "room": {
+      "ID": "3",
+      "room_description": "3 beds with a view",
+      "location_id": "3",
+      "number_of_beds": "3",
+      "room_price": "3500",
+      "location": {
+        "ID": "3",
+        "name": "Premier Inn London Leicester Square",
+        "lat": "51.51104650000001",
+        "lng": "-0.13018379999994067",
+        "google_id": "ChIJJUyxFNIEdkgRajb-0tlYT_0",
+        "street_number": "1",
+        "address_line1": "Leicester Square",
+        "town": "London",
+        "postcode": "WC2H 7BP",
+        "country": "United Kingdom",
+        "location_type": "Hotel",
+        "belongs_to": "1"
+      }
+    }
+  }
+}
+            </pre>
+            <h1>And an example error:</h1>
+            <pre class="prettyprint">
+{
+  "error": "A booking already exists within this period",
+  "message": "Booking failed",
+  "errorCode": 400
+}
+            </pre>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>3) Places to stay should utilise AJAX front end - <a href="#searchContainer" data-scroll>View here</a></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>
+                4) Visit Colorado should be able to look up locations using API
+                - <a href="http://visitcolorado.alexpotter.dev/">View here</a>
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>5) Places to stay should show users local area - <a href="#searchContainer" data-scroll>View here</a></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>6) Display results as markers and text - <a href="#searchContainer" data-scroll>View here</a></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>7) Visit Visit Colorado should be able to make bookings using API - <a href="http://visitcolorado.alexpotter.dev/">View here</a></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>8) When the user clicks on a marker a pop up of text displays explaining location - <a href="#searchContainer" data-scroll>View here</a></h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1>9) Interactive calendar should be implemented - <a href="#searchContainer" data-scroll>View here</a></h1>
         </div>
     </div>
 </div>
